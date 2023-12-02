90a53b6352f796552fec8fedfdfc9798c865fd57
Daniel Danner <daniel.danner@inovex.de>
2023-12-03 00:00:14 +0100

    Add null check to GetMethod

--- AoC2023/Program.cs
+++ AoC2023/Program.cs
@@ -8,8 +8,8 @@ foreach (var day in days)
     var inputFile = $"inputs/{day.Name.ToLower()}.txt";
     var input = File.ReadAllText(inputFile);
 
-    var part1 = day.GetMethod("Part1").Invoke(null, new object?[] { input });
-    var part2 = day.GetMethod("Part2").Invoke(null, new object?[] { input });
+    var part1 = day.GetMethod("Part1")?.Invoke(null, new object?[] { input });
+    var part2 = day.GetMethod("Part2")?.Invoke(null, new object?[] { input });
     
     Console.WriteLine($"{day.ToString()} part 1: {part1}");
     Console.WriteLine($"{day.ToString()} part 1: {part2}");

8f1d4f8a9b794757fab29ffdb0796bae675f65a1
Daniel Danner <daniel.danner@inovex.de>
2023-12-03 14:30:49 +0100

    Add day02, part2

--- AoC2023/Day02.cs
+++ AoC2023/Day02.cs
@@ -1,6 +1,3 @@
-using System.ComponentModel.Design;
-using System.Reflection;
-using System.Text.RegularExpressions;
 using FluentAssertions;
 using Xunit;
 
@@ -8,7 +5,10 @@ namespace AoC2023;
 
 public class Day02 : IDay
 {
-    public record CubeSet(int Red, int Green, int Blue);
+    public record CubeSet(int Red, int Green, int Blue)
+    {
+        public int Power => Red * Green * Blue;
+    }
 
     public record Game(int Id, List<CubeSet> CubeSets);
     
@@ -29,7 +29,14 @@ public class Day02 : IDay
 
     public static string Part2(string input)
     {
-        return "";
+        var sumOfPowers = 0;
+        foreach (var gameLine in Helpers.Lines(input))
+        {
+            var game = ParseGame(gameLine);
+            sumOfPowers += MinCubeSet(game).Power;
+        }
+
+        return sumOfPowers.ToString();
     }
 
     public static Game ParseGame(string line)
@@ -58,10 +65,31 @@ public class Day02 : IDay
         return new Game(gameId, cubeSets);
     }
 
+    public static CubeSet MinCubeSet(Game game)
+    {
+        int minRed = 0, minGreen = 0, minBlue = 0;
+        foreach (var cubeSet in game.CubeSets)
+        {
+            minRed = int.Max(minRed, cubeSet.Red);
+            minGreen = int.Max(minGreen, cubeSet.Green);
+            minBlue = int.Max(minBlue, cubeSet.Blue);
+        }
+
+        return new CubeSet(minRed, minGreen, minBlue);
+    }
+
+    private readonly string _commonSample = """
+                                            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                                            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                                            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                                            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                                            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+                                            """;
+
     [Fact]
     public void ParseGame_SampleGame_ParsedCorrectly()
     {
-        var line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        const string line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
         var game = ParseGame(line);
 
         game.Id.Should().Be(1);
@@ -74,13 +102,21 @@ public class Day02 : IDay
     [Fact]
     public void Part1_SampleInput_CorrectResult()
     {
-        var input = """
-                    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-                    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-                    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-                    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-                    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-                    """;
-        Part1(input).Should().Be("8");
+        Part1(_commonSample).Should().Be("8");
+    }
+
+    [Fact]
+    public void Part2_SampleInput_CorrectResult()
+    {
+        Part2(_commonSample).Should().Be("2286");
+    }
+
+    [Fact]
+    public void MinCubeSet_SampleInput_CorrectResult()
+    {
+        const string line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        var minCubeSet = MinCubeSet(ParseGame(line));
+
+        minCubeSet.Should().BeEquivalentTo(new CubeSet(4, 2, 6));
     }
 }
